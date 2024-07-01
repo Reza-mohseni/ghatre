64086dc2371de6ee7f31ab12d00237d4a9799810
abolfazlmohseni <131040476+abolfazlmohseni@users.noreply.github.com>
2024-07-01 12:13:27 +0330

    login and regester final

--- app/views/assets/Login&SinUp/js/login-script.js
+++ app/views/assets/Login&SinUp/js/login-script.js
@@ -15,3 +15,36 @@ signupLink.onclick = () => {
   signupBtn.click();
   return false;
 };
+
+const togglePassword1 = document.querySelector('#togglePassword1');
+const password1 = document.querySelector('#password1');
+
+togglePassword1.addEventListener('click', function (e) {
+    const type = password1.getAttribute('type') === 'password' ? 'text' : 'password';
+    password1.setAttribute('type', type);
+    
+    this.classList.toggle('bi-eye');
+    this.classList.toggle('bi-eye-slash');
+});
+
+const togglePassword2 = document.querySelector('#togglePassword2');
+const password2 = document.querySelector('#password2');
+
+togglePassword2.addEventListener('click', function (e) {
+    const type = password2.getAttribute('type') === 'password' ? 'text' : 'password';
+    password2.setAttribute('type', type);
+    
+    this.classList.toggle('bi-eye');
+    this.classList.toggle('bi-eye-slash');
+});
+
+const togglePassword3 = document.querySelector('#togglePassword3');
+const password3 = document.querySelector('#password3');
+
+togglePassword3.addEventListener('click', function (e) {
+    const type = password3.getAttribute('type') === 'password' ? 'text' : 'password';
+    password3.setAttribute('type', type);
+    
+    this.classList.toggle('bi-eye');
+    this.classList.toggle('bi-eye-slash');
+});
