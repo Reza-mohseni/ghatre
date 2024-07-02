e6a5982b852420bfd4912f212caf0ccdcbfa4b3d
abolfazlmohseni <131040476+abolfazlmohseni@users.noreply.github.com>
2024-07-02 19:05:00 +0330

    Counter final
    پیشخوان

--- app/views/assets/Counter/js/Counter.js
+++ app/views/assets/Counter/js/Counter.js
@@ -1,37 +1,48 @@
-$(document).ready(function () {
-  $.ajax({
-    url: 'data.php',
-    method: 'GET',
-    success: function (response) {
-      console.log("Data received from PHP: ", response); // چاپ داده‌ها در کنسول
 
-      var ctx = document.getElementById("Chart").getContext("2d");
-      var myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-          labels: response.labels,
-          datasets: [{
-            label: 'رطوبت خاک',
-            data: response.data,
-            backgroundColor: 'rgba(13, 110, 253, 0.3)',
-            borderColor: '#7A89FF',
-            borderWidth: 3,
-            hoverBorderWidth: 3.5,
-          }]
-        },
-        options: {
-          responsive: false,
-          scales: {
-            xAxes: [{
-              distribution: 'linear'
-            }]
-          }
-        }
-      });
+$(document).ready(function () {
+  var ctx = document.getElementById("Chart").getContext("2d");
+  var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: [],
+      datasets: [{
+        label: 'رطوبت خاک',
+        data: [],
+        backgroundColor: 'rgba(13, 110, 253, 0.3)',
+        borderColor: '#7A89FF',
+        borderWidth: 3,
+        hoverBorderWidth: 3.5,
+      }]
     },
-    error: function(jqXHR, textStatus, errorThrown) {
-      console.error("AJAX Error: ", textStatus, errorThrown);
+    options: {
+      responsive: false,
+      scales: {
+        xAxes: [{
+          distribution: 'linear'
+        }]
+      }
     }
   });
-});
 
+  function updateChart() {
+    $.ajax({
+      url: 'data.php',
+      method: 'GET',
+      success: function (response) {
+        console.log("Data received from PHP: ", response);
+
+        myChart.data.labels = response.labels;
+        myChart.data.datasets[0].data = response.data;
+        myChart.update(); 
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        console.error("AJAX Error: ", textStatus, errorThrown);
+      }
+    });
+  }
+
+  updateChart();
+
+
+  setInterval(updateChart, 15000);
+});
