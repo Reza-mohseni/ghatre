76b8c2be17e9535b19718483535dd6c1ab08457c
Reza Mohseni <mohseni.rezaofficial@gmail.com>
2024-07-06 14:45:40 +0330

    تست

--- app/views/Login&SinUp/login.php
+++ app/views/Login&SinUp/login.php
@@ -1,8 +1,5 @@
 <?php
-require_once '../../core/healper/healper.php';
-if (!isset($_SESSION['name'])){
-    redirect('app/views/Login&SinUp/login.php');
-}
+
 ?>
 
 <!DOCTYPE html>
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'app/core/healper.php';
+require_once 'app/core/healper/healper.php';
 if (!isset($_SESSION['name'])){
     redirect('app/views/Login&SinUp/login.php');
 }
